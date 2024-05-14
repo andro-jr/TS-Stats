8ef9afea2121905dbb4487a378842df6c21a7e8d
andro-jr <prabin20panta@gmail.com>
2024-05-14 22:14:05 +0545

    modified file read structure

--- build/index.js
+++ build/index.js
@@ -1,15 +1,21 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default
-    .readFileSync('./football.csv', {
-    encoding: 'utf-8',
-})
-    .split('\n')
-    .map((item) => {
-    return item.split(',');
-});
-console.log(matches);
+const CsvFileReader_1 = require("./CsvFileReader");
+const footballCsv = new CsvFileReader_1.CsvFileReader('football.csv');
+footballCsv.read();
+let manUnitedWins = 0;
+var MatchResults;
+(function (MatchResults) {
+    MatchResults["HomeWin"] = "H";
+    MatchResults["AwayWin"] = "A";
+    MatchResults["Draw"] = "D";
+})(MatchResults || (MatchResults = {}));
+for (let match of footballCsv.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log('manUnitedWins :', manUnitedWins);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('./football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((item: string): string[] => {
-    return item.split(',');
-  });
+const footballCsv = new CsvFileReader('football.csv');
 
-console.log(matches);
+footballCsv.read();
+
+let manUnitedWins = 0;
+
+enum MatchResults {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+for (let match of footballCsv.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log('manUnitedWins :', manUnitedWins);
