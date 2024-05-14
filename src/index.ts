04a87abf721fa746d1739288f4eabcb30a7f2bf8
andro-jr <prabin20panta@gmail.com>
2024-05-14 21:29:07 +0545

    csv file and reading the data and parse

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('test');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('./football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((item: string): string[] => {
+    return item.split(',');
+  });
+
+console.log(matches);
