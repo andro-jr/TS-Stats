ee1b355ec0164fe026dd5d7850914df82d5602e1
andro-jr <prabin20panta@gmail.com>
2024-05-15 16:35:38 +0545

    refactored with use of interface instead of inheritance approach

--- build/index.js
+++ build/index.js
@@ -1,16 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const CsvFileReader_1 = require("./CsvFileReader");
+const MatchReader_1 = require("./MatchReader");
 const MathResult_1 = require("./MathResult");
-const footballCsv = new CsvFileReader_1.CsvFileReader('football.csv');
+const footballCsv = new CsvFileReader_1.CsvFileReader("football.csv");
+const footballMatchReader = new MatchReader_1.MatchReader(footballCsv);
+footballMatchReader.load();
 footballCsv.read();
 let manUnitedWins = 0;
-for (let match of footballCsv.data) {
-    if (match[1] === 'Man United' && match[5] === MathResult_1.MatchResults.HomeWin) {
+for (let match of footballMatchReader.matches) {
+    if (match[1] === "Man United" && match[5] === MathResult_1.MatchResults.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === MathResult_1.MatchResults.AwayWin) {
+    else if (match[2] === "Man United" && match[5] === MathResult_1.MatchResults.AwayWin) {
         manUnitedWins++;
     }
 }
-console.log('manUnitedWins :', manUnitedWins);
+console.log("manUnitedWins :", manUnitedWins);
--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,7 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResults } from './MathResult';
-
-type MatchData = [Date, string, string, number, number, MatchResults, string];
 
 export class CsvFileReader {
-  data: MatchData[] = [];
+  data: string[][] = [];
 
   constructor(public filename: string) {}
 
@@ -17,17 +13,6 @@ export class CsvFileReader {
       .split('\n')
       .map((item: string): string[] => {
         return item.split(',');
-      })
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResults,
-          row[6],
-        ];
       });
   }
 }
