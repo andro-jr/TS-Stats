f4b0594fcd58273940cb505d61613abb8fe85d70
andro-jr <prabin20panta@gmail.com>
2024-05-15 16:14:19 +0545

    Revert "modified file read structure"
    This reverts commit 8ef9afea2121905dbb4487a378842df6c21a7e8d.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-import { MatchReader } from "./MatchReader";
-import { MatchResults } from "./MathResult";
+import fs from "fs";
 
-const footballCsv = new MatchReader("football.csv");
+const matches = fs
+  .readFileSync("./football.csv", {
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((item: string): string[] => {
+    return item.split(",");
+  });
 
-footballCsv.read();
-
-let manUnitedWins = 0;
-
-for (let match of footballCsv.data) {
-  if (match[1] === "Man United" && match[5] === MatchResults.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResults.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log("manUnitedWins :", manUnitedWins);
+console.log(matches);
